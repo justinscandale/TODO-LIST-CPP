6126ad198a6049a7aff3740b5db9944cfd8bb6cb
Tsega <tsega207@gmail.com>
2024-04-21 15:52:09 -0400

    User interface

--- main.cpp
+++ main.cpp
@@ -1,41 +1,178 @@
+// #include "event.cpp"
+// #include "priorityQueue.cpp"
+// #include <iostream>
+// #include <string>
+
+// // main used for testing
+// int main()
+// {
+//     // EVENT myEvent = EVENT("NAME_HERE",999,"INFORMATION_HERE",1,02042025);
+
+//     // //tests for event methods + print out map contents
+//     // myEvent.toggleComplete();
+//     // std::map<std::string,std::string> output = myEvent.eventToDictionary();
+//     // for(auto& t: output)
+//     // {
+//     //     std::cout<< t.first << " "
+//     //         <<t.second << "\n";
+//     // }
+
+//     // test methods for priority queue
+//     // PriorityQueue pq = PriorityQueue();
+//     // pq.addEvent("EXAMPLE1", "deets1", 1, 20000101);
+//     // pq.addEvent("EXAMPLE2", "deets2", 1, 20010101);
+//     // pq.addEvent("EXAMPLE3", "deets3", 1, 20010101);
+//     // pq.addEvent("EXAMPLE4", "deets4", 1, 20000102);
+//     // pq.removeEvent(2);
+//     // pq.printPriorityQueue();
+
+//     // // print out vector of maps
+//     // std::vector<std::map<std::string, std::string>> temp = pq.viewEvents();
+//     // std::cout << "\n\n";
+//     // for (size_t i = 0; i < temp.size(); i++)
+//     // {
+//     //     std::cout << i << "th Event in PRIORIRTY QUEUE\n";
+//     //     for (auto &t : temp[i])
+//     //     {
+//     //         std::cout << t.first << " "
+//     //                   << t.second << "\n";
+//     //     }
+//     //     std::cout << "\n\n";
+//     // }
+//     PriorityQueue pq;
+//     int choice;
+//     std::string eventName, eventDetails;
+//     int eventPriority, eventDate;
+
+//     while (true)
+//     {
+//         std::cout << "\nPlease choose an option:\n";
+//         std::cout << "1. Add event\n";
+//         std::cout << "2. Remove event\n";
+//         std::cout << "3. View events\n";
+//         std::cout << "4. Exit\n";
+//         std::cout << "Enter your choice: ";
+//         std::cin >> choice;
+
+//         switch (choice)
+//         {
+//         case 1:
+//             std::cout << "Enter event name: ";
+//             std::cin >> eventName;
+//             std::cout << "Enter event details: ";
+//             std::cin >> eventDetails;
+//             std::cout << "Enter event priority: ";
+//             std::cin >> eventPriority;
+//             std::cout << "Enter event date (YYYYMMDD): ";
+//             std::cin >> eventDate;
+//             pq.addEvent(eventName, eventDetails, eventPriority, eventDate);
+//             break;
+//         case 2:
+//             std::cout << "Enter event priority to remove: ";
+//             std::cin >> eventPriority;
+//             pq.removeEvent(eventPriority);
+//             break;
+//         case 3:
+//             std::vector<std::map<std::string, std::string>> temp = pq.viewEvents();
+//             for (size_t i = 0; i < temp.size(); i++)
+//             {
+//                 std::cout << i << "th Event in PRIORITY QUEUE\n";
+//                 for (auto &t : temp[i])
+//                 {
+//                     std::cout << t.first << " " << t.second << "\n";
+//                 }
+//                 std::cout << "\n\n";
+//             }
+//             break;
+//         case 4:
+//             std::cout << "Exiting...\n";
+//             return 0;
+//         default:
+//             std::cout << "Invalid choice. Please choose a valid option.\n";
+//         }
+//     }
+
+//     return 0;
+// }
+
 #include "event.cpp"
+#include <iostream>
+#include <string>
 #include "priorityQueue.cpp"
 
-//main used for testing 
 int main()
 {
-    //EVENT myEvent = EVENT("NAME_HERE",999,"INFORMATION_HERE",1,02042025);
-    
-    // //tests for event methods + print out map contents
-    // myEvent.toggleComplete();
-    // std::map<std::string,std::string> output = myEvent.eventToDictionary();
-    // for(auto& t: output)
-    // {
-    //     std::cout<< t.first << " "
-    //         <<t.second << "\n";
-    // }
-
-    //test methods for priority queue
     PriorityQueue pq = PriorityQueue();
-    pq.addEvent("EXAMPLE1","deets1",1,20000101);
-    pq.addEvent("EXAMPLE2","deets2",1,20010101);
-    pq.addEvent("EXAMPLE3","deets3",1,20010101);
-    pq.addEvent("EXAMPLE4","deets4",1,20000102);
-    pq.removeEvent(2);
-    pq.printPriorityQueue();
+    int choice;
+    std::string eventName, eventDetails;
+    int eventPriority, eventDate, eventUniqueID;
+    std::vector<std::map<std::string, std::string>> temp;
 
-    //print out vector of maps
-    std::vector<std::map<std::string,std::string>> temp = pq.viewEvents();
-    std::cout<<"\n\n";
-    for(size_t i = 0; i<temp.size(); i++)
+    while (true)
     {
-        std::cout<< i << "th Event in PRIORIRTY QUEUE\n";
-        for(auto& t: temp[i])
+        std::cout << "\nPlease choose an option:\n";
+        std::cout << "1. Add event\n";
+        std::cout << "2. Remove event\n";
+        std::cout << "3. View events\n";
+        std::cout << "4. Exit\n";
+        std::cout << "Enter your choice: ";
+        std::cin >> choice;
+
+        // Clear the newline character left in the buffer
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        std::cout << "\n";
+
+        switch (choice)
         {
-            std::cout<< t.first << " "
-                <<t.second << "\n";
+        case 1:
+            std::cout << "Enter event name: ";
+            std::getline(std::cin, eventName);
+            std::cout << std::endl;
+
+            std::cout << "Enter event details: ";
+            std::getline(std::cin, eventDetails);
+            std::cout << std::endl;
+
+            std::cout << "Enter event priority: ";
+            std::cin >> eventPriority;
+            std::cout << std::endl;
+
+            std::cout << "Enter event date (YYYYMMDD): ";
+            std::cin >> eventDate;
+            std::cout << std::endl;
+
+            pq.addEvent(eventName, eventDetails, eventPriority, eventDate);
+            std::cout << "Event added successfully.\n";
+            break;
+        case 2:
+            std::cout << "Enter unique ID of event to remove: ";
+            std::cin >> eventUniqueID;
+            pq.removeEvent(eventUniqueID);
+            break;
+        case 3:
+            temp = pq.viewEvents();
+            if (temp.size() == 0)
+            {
+                std::cout << "No events on the list.\n";
+                break;
+            }
+            for (size_t i = 0; i < temp.size(); i++)
+            {
+                std::cout << i + 1 << "th Event on the TODO list\n";
+                for (auto &t : temp[i])
+                {
+                    std::cout << t.first << " " << t.second << "\n";
+                }
+                std::cout << "\n\n";
+            }
+            break;
+        case 4:
+            std::cout << "Exiting...\n";
+            return 0;
+        default:
+            std::cout << "Invalid choice. Please choose a valid option.\n";
         }
-        std::cout<<"\n\n";
     }
 
     return 0;
--- priorityQueue.cpp
+++ priorityQueue.cpp
@@ -1,29 +1,29 @@
 #include "priorityQueue.hpp"
 
-//put each event into map of <string, string> and insert this into a vector
-std::vector<std::map<std::string,std::string>> PriorityQueue::viewEvents()  
+// put each event into map of <string, string> and insert this into a vector
+std::vector<std::map<std::string, std::string>> PriorityQueue::viewEvents()
 {
-    std::vector<std::map<std::string,std::string>> temp;
-    
-    for(EVENT* cur = root; cur!= nullptr; cur=cur->next)
+    std::vector<std::map<std::string, std::string>> temp;
+
+    for (EVENT *cur = root; cur != nullptr; cur = cur->next)
     {
         temp.push_back(cur->eventToDictionary());
     }
 
     return temp;
-};  
+};
 
-//add event to priority queue
+// add event to priority queue
 void PriorityQueue::addEvent(std::string name, std::string info, int priorityLevel, int priorityDate)
 {
-    EVENT *newEvent = new EVENT(name,uniqueID++,info,priorityLevel,priorityDate);
+    EVENT *newEvent = new EVENT(name, uniqueID++, info, priorityLevel, priorityDate);
 
-    //insert at beginning if empty
-    if(isEmpty())
+    // insert at beginning if empty
+    if (isEmpty())
     {
         root = newEvent;
     }
-    //insert in sorted order if not empty
+    // insert in sorted order if not empty
     else
     {
         insertEventByDate(newEvent);
@@ -32,67 +32,69 @@ void PriorityQueue::addEvent(std::string name, std::string info, int priorityLev
     size++;
 
     return;
-};  
+};
 
-//remove event from priority queue based on uniqueID
+// remove event from priority queue based on uniqueID
 void PriorityQueue::removeEvent(int uniqueID)
 {
-    if(isEmpty()) {return;};
+    if (isEmpty())
+    {
+        return;
+    };
 
-    //search for event
-    EVENT* prev = nullptr;
-    EVENT* cur = root;
+    // search for event
+    EVENT *prev = nullptr;
+    EVENT *cur = root;
 
-    while(cur != nullptr && cur->getUniqueID() != uniqueID)
+    while (cur != nullptr && cur->getUniqueID() != uniqueID)
     {
         prev = cur;
         cur = cur->next;
     }
 
-    //remove EVENT from beginning of priority queue
-    if(prev==nullptr)
+    // remove EVENT from beginning of priority queue
+    if (prev == nullptr)
     {
         root = root->next;
     }
-    //remove EVENT from anywhere else in priority queue
-    else if(cur!=nullptr)
+    // remove EVENT from anywhere else in priority queue
+    else if (cur != nullptr)
     {
         prev->next = cur->next;
-        delete(cur);
+        delete (cur);
     }
-    //EVENT not found
+    // EVENT not found
     else
     {
         return;
     }
+};
 
-}; 
-
-//sort priority queue based on priority level attribute then priority date attribute
+// sort priority queue based on priority level attribute then priority date attribute
 void PriorityQueue::sortByPriority()
 {
     return;
-}; 
+};
 
-//return true if priority queue is empty
+// return true if priority queue is empty
 bool PriorityQueue::isEmpty()
 {
     return size == 0 ? true : false;
 };
 
-//insert EVENT in prioirty queue in order of date then prioirty 
+// insert EVENT in priority queue in order of date then prioirty
 void PriorityQueue::insertEventByDate(EVENT *ptr)
-{   
+{
     EVENT *cur = root;
     EVENT *prev = nullptr;
 
-    while(cur!=nullptr && ptr->getPriorityDate()>cur->getPriorityDate())
+    while (cur != nullptr && ptr->getPriorityDate() > cur->getPriorityDate())
     {
         prev = cur;
         cur = cur->next;
     }
 
-    if(prev == nullptr)
+    if (prev == nullptr)
     {
         ptr->next = root;
         root = ptr;
@@ -104,14 +106,14 @@ void PriorityQueue::insertEventByDate(EVENT *ptr)
     }
 };
 
-//print prioirity queue
+// print priority queue
 void PriorityQueue::printPriorityQueue()
 {
-    for(EVENT *cur = root; cur!=nullptr; cur=cur->next)
-    {   
-        std::cout<<cur->getName()<<"  "<<cur->getInfo()<<"  "
-        <<std::to_string(cur->getPriorityDate())<<"  "
-        <<std::to_string(cur->getUniqueID()) <<std::endl;
+    for (EVENT *cur = root; cur != nullptr; cur = cur->next)
+    {
+        std::cout << cur->getName() << "  " << cur->getInfo() << "  "
+                  << std::to_string(cur->getPriorityDate()) << "  "
+                  << std::to_string(cur->getUniqueID()) << std::endl;
     }
     return;
 };
